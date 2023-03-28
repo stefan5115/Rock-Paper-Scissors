47f282a667c0297100cc283092159ce413dc4d9e
Stefan Tanase <111762429+stefan5115@users.noreply.github.com>
2023-03-28 14:33:51 +0300

    Am termiant functia game am adaugat niste variabile si toLowerCase() la playerSlection

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let scorePlayer = 0
+let scoreComputer = 0
+let tied = 0
+
 function getComputerChoice(){
     let choice = ['rock','paper','scissors']
     let randomNumber = Math.floor(Math.random() * 3)
@@ -6,29 +10,40 @@ function getComputerChoice(){
 
 function playRound (playerSelection, computerSelection){
     if(playerSelection.toLowerCase() === computerSelection){
+        tied++
         return "It's a tied"
-    }else if (playerSelection === 'rock' && computerSelection === "paper"){
+    }else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "paper"){
+        scoreComputer++
         return "Computer Win!"
-    }else if (playerSelection === 'rock' && computerSelection === "scissors"){
+    }else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "scissors"){
+        scorePlayer++
         return "You Win!"
-    }else if (playerSelection === 'paper' && computerSelection === "rock"){
+    }else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "rock"){
+        scorePlayer++
         return "You Win!"
-    }else if (playerSelection === 'paper' && computerSelection === "scissors"){
+    }else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "scissors"){
+        scoreComputer++
         return "Computer Win!"
-    }else if (playerSelection === 'scissors' && computerSelection === "paper"){
+    }else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "paper"){
+        scorePlayer++
         return "You Win!"
-    }else if (playerSelection === 'scissors' && computerSelection === "rock"){
+    }else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "rock"){
+        scoreComputer++
         return "Computer Win!"
     }
 }
 
-let player = 'paper';
-let computer = getComputerChoice()
-console.log(playRound(player,computer))
-
 function game(){
     for(let i = 0; i < 5 ; i++){
-        return playRound(player,computer)
+        let player = prompt("Ce alegi: ROCK | PAPER | SCISSORS ?")
+        let computer = getComputerChoice()
+        console.log(playRound(player,computer)) 
+        console.log(`Player:${scorePlayer} Tied:${tied} Computer${scoreComputer}`)
+    }
+    if(scorePlayer > scoreComputer){
+        console.log("Player win!!")
+    }else{
+        console.log("Computer win!!")
     }
 }
 
