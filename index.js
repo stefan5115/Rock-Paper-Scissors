3b2a7587cd8f3c1426519743e9f97b27c3d9989f
Stefan Tanase <111762429+stefan5115@users.noreply.github.com>
2023-03-28 14:18:33 +0300

    Am creat functia game si am creat alte optiuni

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ function getComputerChoice(){
     return choice[randomNumber]
 }
 
-function playRound (playerSlection, computerSelection){
-    if(playerSlection.toLowerCase() === computerSelection){
+function playRound (playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() === computerSelection){
         return "It's a tied"
+    }else if (playerSelection === 'rock' && computerSelection === "paper"){
+        return "Computer Win!"
+    }else if (playerSelection === 'rock' && computerSelection === "scissors"){
+        return "You Win!"
+    }else if (playerSelection === 'paper' && computerSelection === "rock"){
+        return "You Win!"
+    }else if (playerSelection === 'paper' && computerSelection === "scissors"){
+        return "Computer Win!"
+    }else if (playerSelection === 'scissors' && computerSelection === "paper"){
+        return "You Win!"
+    }else if (playerSelection === 'scissors' && computerSelection === "rock"){
+        return "Computer Win!"
     }
 }
 
-let player = 'rock';
+let player = 'paper';
 let computer = getComputerChoice()
 console.log(playRound(player,computer))
 
+function game(){
+    for(let i = 0; i < 5 ; i++){
+        return playRound(player,computer)
+    }
+}
+
+game()
